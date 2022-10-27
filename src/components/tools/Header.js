bf4740d5e068df80d6e89b209784196ff900693c
des9tka <des9tka2005@ukr.net>
2022-10-27 22:50:04 +0300

    com

--- src/components/tools/Header.js
+++ src/components/tools/Header.js
@@ -6,15 +6,18 @@ import {PageCounter} from "./PageCounter";
 const Header = () => {
     let navigate = useNavigate();
     return(
-        <div className={'Header'}>
-            <button className={'hBtn1'} onClick={()=> navigate('/')}>Home</button>
-            <button className={'hBtn2'} onClick={()=> navigate(`/movies`)}>All movies</button>
-            <Switcher/>
-            <span className={'field'}></span>
-            <PageCounter/>
-            <div className={'moon'}><i className="fa-regular fa-moon-stars"></i></div>
+        <div>
+            <div className={'Header'}>
+                <button className={'hBtn2'} onClick={()=> navigate(`/movies`)}>All movies</button>
+                <button className={'hBtn1'} onClick={()=> navigate('/')}>Home</button>
+                <Switcher/>
+                <span className={'field'}></span>
+                <div className={'moon'}><i className="fa-regular fa-moon-stars"></i></div>
+                <PageCounter/>
+            </div>
         </div>
-    )
+
+)
 }
 
 export {
--- src/components/tools/PageCounter.js
+++ src/components/tools/PageCounter.js
@@ -3,7 +3,7 @@ import {useSelector} from "react-redux";
 const PageCounter = () => {
     const {page} = useSelector(state => state.movies)
     return (
-        <div>
+        <div className={'PAGE'}>
             <h3 className={'PageCounter'}>PAGE : {page}</h3>
         </div>
     )
